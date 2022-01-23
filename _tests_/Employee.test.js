9ec42f8e250d9c5e2f8347373811f9c73977c93f
mflanigan <mflanigantwualumn@gmail.com>
2022-01-23 16:57:32 -0600

    updated objects and added prompts to index

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,6 +1,21 @@
-// properties: name, ID, Email
+// using Employee constructor 
+const Employee = require('../lib/Employee');
+
+// creates employee object 
+test('creates an employee object', () => {
+    const employee = new Employee
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
 
 // getName()
+test('gets employee name', () => {
+    const employee = New Employee
+
+    expect(employee.getName()).toEqual(expect.any(String));
+})
 
 // getID()
 
