8641ee6eccfe7456b304aeaad9b900b513c092ef
mflanigan <mflanigantwualumn@gmail.com>
2022-01-24 18:08:51 -0600

    working on tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+// properties: name, ID, Email
+const Engineer = require('../lib/Employee');
+
+// github // github user name
+// test("Gets github name through github method", () => {
+//     const testValue = "mflanigan13"
+//     const e = new Engineer('Mallorie', 123, 'mflanigantwualumn@gmail.com', testValue);
+//     expect(e.getGithub()).toBe(testValue);
+// })
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "mflanigan13";
+    const employeeInstance = new Employee("Mallorie", 123, "example@gmail.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
+});
+
+// getRole() // overridden to return 'Engineer'
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeInstance = new Employee("Mallorie", 123, "example@gmail.com", "githubex");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+});
