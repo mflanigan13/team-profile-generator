5b5ec2ef4447558c854e3af03a54cb167a67850a
mflanigan <mflanigantwualumn@gmail.com>
2022-01-27 21:02:24 -0600

    updating CSS to match mock-up

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,9 +5,9 @@ const generateTeamPage = function (team) {
         return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            <div class="card-header bg-primary">
+                <h3 class="text-white">${manager.name}</h3>
+                <h4 class="text-white">Manager</h4><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
@@ -24,9 +24,9 @@ const generateTeamPage = function (team) {
         return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            <div class="card-header bg-primary">
+                <h3 class="text-white">${engineer.name}</h3>
+                <h4 class="text-white">Engineer</h4><i class="material-icons">laptop_mac</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${engineer.id}</p>
@@ -43,9 +43,9 @@ const generateTeamPage = function (team) {
         return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            <div class="card-header bg-primary">
+                <h3 class="text-white">${intern.name}</h3>
+                <h4 class="text-white">Intern</h4><i class="material-icons">assignment_ind</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
@@ -61,18 +61,18 @@ const generateTeamPage = function (team) {
     const html = [];
     html.push(
         team.filter(employee => employee.getRole() === 'Manager')
-        .map(manager => generateManager(manager))
-        .join('')
+            .map(manager => generateManager(manager))
+            .join('')
     )
     html.push(
         team.filter(employee => employee.getRole() === 'Engineer')
-        .map(engineer => generateEngineer(engineer))
-        .join('')
+            .map(engineer => generateEngineer(engineer))
+            .join('')
     )
     html.push(
         team.filter(employee => employee.getRole() === 'Intern')
-        .map(intern => generateIntern(intern))
-        .join('')
+            .map(intern => generateIntern(intern))
+            .join('')
     )
 
     return html.join('')
@@ -82,36 +82,33 @@ const generateTeamPage = function (team) {
 module.exports = team => {
     return `
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Profile</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <header>
-            <nav class="navbar" id="navbar">
-                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
-            </nav>
-        </header>
-        <main>
-            <div class="container">
-                <div class="row justify-content-center" id="team-cards">
-                    <!--Team Cards-->
-                    ${generateTeamPage(team)}
-                </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+                <h1 class="text-center text-white">My Team</h1>
             </div>
-        </main>
-    
-    </body>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    </html>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="row team-area col-12 d-flex justify-content-center">
+            ${generateTeamPage(team)}  
+            </div>
+        </div>
+    </div>
+</body>
+</html>
 `
 }
